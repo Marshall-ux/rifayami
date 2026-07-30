807d6208cc8f525aba9e3877fea4f5e649576a69
yamilaanestiades-code <yamila.anestiades@gmail.com>
2026-07-29 22:35:30 -0300

    Update config.js

--- config.js
+++ config.js
@@ -64,9 +64,9 @@ const CONFIG = {
      ====================================================================== */
 
   // PAGADOS (se ven en gris y no se pueden elegir)
-  vendidos: [12, 17],
+  vendidos: [6,7,12,15,17,24],
 
   // RESERVADOS, esperando el pago (se ven en amarillo y no se pueden elegir)
-  reservados: [],
+  reservados: [1,304,304,444,456,501,508],
 
 };
